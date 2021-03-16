32d66ec03ba0e490ee934d626a27343712eab612
Hal Deadman <hal.deadman@gmail.com>
2021-03-15 23:15:24 -0400

    specify log4j config

--- run-cas.sh
+++ run-cas.sh
@@ -47,7 +47,7 @@ sed -i 's/\/var\/log/.\/logs/g' ./etc/cas/config/log4j2.xml
 
 # Run CAS server using arguments for config rather than property files, make config folders and certs relative to project to avoid needing to use sudo
 echo "Running CAS Server"
-java -jar build/libs/cas.war \
+java -Dlog4j.configurationFile=./etc/cas/config/log4j2.xml -jar build/libs/cas.war \
 	--server.ssl.key-store=thekeystore \
   --spring.profiles.active=standalone,ldap \
 	--cas.standalone.configuration-directory=./config \
